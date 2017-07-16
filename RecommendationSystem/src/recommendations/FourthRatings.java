f61ddd3a52711e6214a82bbbccf3692c54d80275
Prasandeep Singh <prasan.ubhi@gmail.com>
2017-07-16 14:48:09 -0400

    Recommendation Engine Completed

--- RecommendationSystem/src/recommendations/FourthRatings.java
+++ RecommendationSystem/src/recommendations/FourthRatings.java
@@ -56,80 +56,91 @@ public class FourthRatings {
 //		return ratingList;
 //	}
 //	
-
-    private ArrayList<Rating> getMoviesSortedByWeightedAverages(ArrayList<String> moviesHavingMinRaters, ArrayList<Rating> similarRatings){
+//
+    private ArrayList<Rating> getMoviesSortedByWeightedAverages(ArrayList<String> moviesHavingMinRaters, ArrayList<Rating> similarRatings,
+    		 int numSimilarRaters){
     	ArrayList<Rating> ratingList = new ArrayList<>();
     	for(String currMovieId : moviesHavingMinRaters){
     		ArrayList<Double> weightedRating = new ArrayList<>();
-    		for(Rating currRating : similarRatings){
+    		//for(Rating currRating : similarRatings){
+    		for(int k = 0; k<numSimilarRaters; k++){
+    			Rating currRating = similarRatings.get(k);
     			Rater currRater = RaterDatabase.getRater(currRating.getItem());
-    			weightedRating.add(currRater.getRating(currMovieId) * currRating.getValue());
+    			if(currRater.hasRating(currMovieId))
+    				weightedRating.add(currRater.getRating(currMovieId) * currRating.getValue());
     		}
+    		if(weightedRating.isEmpty()) continue;
     		double sumOfWeightedAverages = 0;
     		for(double wr : weightedRating)
     			sumOfWeightedAverages+=wr;
-    		ratingList.add(new Rating(currMovieId, sumOfWeightedAverages/weightedRating.size()));
+    		
+    		ratingList.add(new Rating(currMovieId,Math.round(sumOfWeightedAverages/weightedRating.size())));
     	}
     	Collections.sort(ratingList, Collections.reverseOrder());
     	return ratingList;
     }
-    
+//    
     public ArrayList<Rating> getSimilarRatingsByFilter(String raterId, int numSimilarRaters, int minimalRaters, Filter criteriaFilter){
     	
     	ArrayList<String> movies = MovieDatabase.filterBy(criteriaFilter);
     	ArrayList<Rating> similarRatings = getSimilarities(raterId);
-    	ArrayList<String> moviesHavingMinRaters = getMoviesHavingMinRaters(movies,minimalRaters, similarRatings, numSimilarRaters);
-    	ArrayList<Rating> ratingList = getMoviesSortedByWeightedAverages(moviesHavingMinRaters, similarRatings);
+    	ArrayList<String> moviesHavingMinRaters = getMoviesHavingMinR(movies,minimalRaters, similarRatings, numSimilarRaters);
+    	ArrayList<Rating> ratingList = getMoviesSortedByWeightedAverages(moviesHavingMinRaters, similarRatings, numSimilarRaters);
     	
     	return ratingList;
     }
-    
-    public ArrayList<Rating> getSimilarRatings(String raterId, int minimalRaters, int numSimilarRaters){
+//    
+    public ArrayList<Rating> getSimilarRatings(String raterId,  int numSimilarRaters, int minimalRaters){
     	ArrayList<String> movies = MovieDatabase.filterBy(new TrueFilter());
     	ArrayList<Rating> similarRatings = getSimilarities(raterId);
-    	ArrayList<String> moviesHavingMinRaters = getMoviesHavingMinRaters(movies,minimalRaters, similarRatings, numSimilarRaters);
-    	ArrayList<Rating> ratingList = getMoviesSortedByWeightedAverages(moviesHavingMinRaters, similarRatings);
+ //   	System.out.println(similarRatings.size());	
+    	ArrayList<String> moviesHavingMinRaters = getMoviesHavingMinR(movies,minimalRaters, similarRatings, numSimilarRaters);
+    	ArrayList<Rating> ratingList = getMoviesSortedByWeightedAverages(moviesHavingMinRaters, similarRatings, numSimilarRaters);
     	return ratingList;
     }
-    
-	   //Returns a list of all movies that have been rated by atleast minimalRaters
-    private ArrayList<String> getMoviesHavingMinRaters(ArrayList<String> movies, int minimalRaters, ArrayList<Rating> similarRaters, int numSimilarRaters){
-    	ArrayList<String> moviesWithMinTopRaters = new ArrayList<>();
-    	HashMap<String, Integer> movieRaterCount = new HashMap<>();
-    	//for(Rating currRating : similarRaters){
-    	for(int i = 0; i<numSimilarRaters; i++){
-    		Rating currRating = similarRaters.get(i);
-    		Rater currRater = RaterDatabase.getRater(currRating.getItem());
-    		ArrayList<String> moviesByCurrRater = currRater.getItemsRated();
-    		for(String currMovie : moviesByCurrRater){
-    			if(movies.contains(currMovie)){
-    				if(!movieRaterCount.containsKey(currMovie)){
-    					movieRaterCount.put(currMovie, 1);
-    				}else{
-    					movieRaterCount.put(currMovie, movieRaterCount.get(currMovie) + 1);
-    				}
-    			}
-    		}
-    	}
-    	
-    	Set<String> movieSet = movieRaterCount.keySet();
-    	for(String movieId : movieSet){
-    		if(movieRaterCount.get(movieId) >= minimalRaters)
-    			moviesWithMinTopRaters.add(movieId);
-    	}
-    	return moviesWithMinTopRaters;
-
-    }
-    
+//    
+//	   //Returns a list of all movies that have been rated by atleast minimalRaters
+//    private ArrayList<String> getMoviesHavingMinRaters(ArrayList<String> movies, int minimalRaters, ArrayList<Rating> similarRatings){
+//    	ArrayList<String> moviesWithMinTopRaters = new ArrayList<>();
+//    	HashMap<String, Integer> movieRaterCount = new HashMap<>();
+//    	for(Rating currRating : similarRatings){
+//    	//for(int i = 0; i<numSimilarRaters; i++){
+//    	//	Rating currRating = similarRaters.get(i);
+//    		Rater currRater = RaterDatabase.getRater(currRating.getItem());
+//    		ArrayList<String> moviesByCurrRater = currRater.getItemsRated();
+//    		for(String currMovie : moviesByCurrRater){
+//    			if(movies.contains(currMovie)){
+//    				if(!movieRaterCount.containsKey(currMovie)){
+//    					movieRaterCount.put(currMovie, 1);
+//    				}else{
+//    					movieRaterCount.put(currMovie, movieRaterCount.get(currMovie) + 1);
+//    				}
+//    			}
+//    		}
+//    	}
+//    	
+//    	Set<String> movieSet = movieRaterCount.keySet();
+//    	for(String movieId : movieSet){
+//    		if(movieRaterCount.get(movieId) >= minimalRaters)
+//    			moviesWithMinTopRaters.add(movieId);
+//    	}
+//    	return moviesWithMinTopRaters;
+//
+//    }
+//    
     private ArrayList<Rating> getSimilarities(String raterIdToBeMatchedWith){
     	ArrayList<Rating> ratingList = new ArrayList<>();
     	Rater me = RaterDatabase.getRater(raterIdToBeMatchedWith);
+    	HashSet<String> setMe = new HashSet<>();
+    	for(String s : me.getItemsRated()){
+    		setMe.add(s);
+    	}
     	for(Rater r : RaterDatabase.getRaters()){
     		double similarityFactor = 0;
     		String currRaterId = r.getID();
     		if(!raterIdToBeMatchedWith.equals(currRaterId)){
-    			similarityFactor = dotProduct(me, r);
-    			if(similarityFactor >= 0 )
+    			similarityFactor = dotProduct(me, r, setMe);
+    			if(similarityFactor > 0 )
     				ratingList.add(new Rating(currRaterId, similarityFactor));
     		}
     	}
@@ -137,47 +148,15 @@ public class FourthRatings {
     	return ratingList;
     }
     
-//    private double dotProduct(Rater me, Rater r){
-//    	int dotP = 0;
-//    
-//    	ArrayList<String> moviesRatedByMe=  me.getItemsRated();
-//    	ArrayList<String> moviesRatedByR = r.getItemsRated();
-//    	ArrayList<Double> meRatingsVector = new ArrayList<>();
-//    	ArrayList<Double> rRatingsVector = new ArrayList<>();
-//
-//    	int i = 0;
-//    	int j = 0;
-//    	while(i<moviesRatedByMe.size() && j<moviesRatedByR.size()){
-//    		String meMovie = moviesRatedByMe.get(i);
-//    		String rMovie = moviesRatedByR.get(j);
-//    		if(meMovie.equals(rMovie)){
-//    			meRatingsVector.add(me.getRating(meMovie)-5);
-//    			rRatingsVector.add(r.getRating(rMovie)-5);
-//    		}else{
-//    			meRatingsVector.add(0.0);
-//    			rRatingsVector.add(0.0);
-//    		}
-//    		i++;
-//    		j++;
-//    	}
-//    	
-//    	for(int k = 0; k<meRatingsVector.size();k++){
-//    		dotP += meRatingsVector.get(k)*rRatingsVector.get(k);
-//    	}
-//    	return dotP;
-////    }
-        private double dotProduct(Rater me, Rater r){
+
+        private double dotProduct(Rater me, Rater r, HashSet<String> setMe){
         	int dotP = 0;
         
-        	ArrayList<String> moviesRatedByMe=  me.getItemsRated();
         	ArrayList<String> moviesRatedByR = r.getItemsRated();
-        	HashSet<String> setMe = new HashSet<>();
         	ArrayList<Double> meRatingsVector = new ArrayList<>();
         	ArrayList<Double> rRatingsVector = new ArrayList<>();
         	
-        	for(String s : moviesRatedByMe){
-        		setMe.add(s);
-        	}
+        	
         	for(int i = 0; i<moviesRatedByR.size();i++){
         		String movieId = moviesRatedByR.get(i);
         		if(setMe.contains(movieId)){
@@ -191,5 +170,62 @@ public class FourthRatings {
         	}
         	return dotP;
         }
+        
+ 	   //Returns a list of all movies that have been rated by atleast minimalRaters
+        private ArrayList<String> getMoviesHavingMinR(ArrayList<String> movies, int minimalRaters, ArrayList<Rating> similarRatings,
+        		 int numSimilarRaters){
+        	ArrayList<String> moviesWithMinTopRaters = new ArrayList<>();
+        	for(String currMovieId : movies){
+        		int numRatersForCurrMovie = numRatingsOfMovie(similarRatings, currMovieId, numSimilarRaters);
+        		if(numRatersForCurrMovie>=minimalRaters)
+        			moviesWithMinTopRaters.add(currMovieId);	
+        	}
+        	return moviesWithMinTopRaters;
+
+        }
+//      //Returns number of raters who rated a particular movie
+    	public int numRatingsOfMovie(ArrayList<Rating> similarRatings, String movieId, int numSimilarRaters){
+    		int ratingCount = 0;
+    		//for(Rating currRating : similarRatings){
+    		for(int i = 0; i<numSimilarRaters; i++){
+    	         Rating currRating = similarRatings.get(i);
+    			Rater currRater = RaterDatabase.getRater(currRating.getItem());
+    			if(currRater.hasRating(movieId))
+    				ratingCount++;
+    		}
+    		return ratingCount;
+    	}
+//        public ArrayList<Rating> getSimilarRatings(String raterId, int numSimilarRaters, int minimalRaters){
+//            ArrayList<Rating> result = new ArrayList<Rating>();
+//            ArrayList<String> movies = MovieDatabase.filterBy(new TrueFilter());
+//            ArrayList<Rating> similarRatings = getSimilarities(raterId);
+//            
+//            for(String movieId : movies){
+//              double weightedAverage = 0;
+//                double sum = 0;
+//                int countRaters = 0;
+//                
+//                for (int i = 0; i < numSimilarRaters; i++) {
+//                  Rating r = similarRatings.get(i);
+//                  String currRaterId = r.getItem();
+//                  double weight = r.getValue();
+//                  
+//                  //get all the movie rated by the rater
+//                  Rater myRater = RaterDatabase.getRater(currRaterId);
+//                if(myRater.hasRating(movieId)) {
+//                  countRaters++;
+//                  sum += weight * myRater.getRating(movieId);
+//                }
+//                }
+//                
+//                if(countRaters >= minimalRaters){
+//                  weightedAverage = sum / countRaters;
+//                  result.add(new Rating(movieId,weightedAverage));
+//                }
+//            }
+//            Collections.sort(result, Collections.reverseOrder());
+//            return result;
+//            
+//        }
 
 }
--- RecommendationSystem/src/recommendations/MovieRunnerSimilarRatings.java
+++ RecommendationSystem/src/recommendations/MovieRunnerSimilarRatings.java
@@ -61,7 +61,7 @@ public class MovieRunnerSimilarRatings {
 		System.out.println("------------------------------------------------------------------");
 		
 		FourthRatings fr = new FourthRatings();
-		ArrayList<Rating> list = fr.getSimilarRatings("65", 5, 20);
+		ArrayList<Rating> list = fr.getSimilarRatings("71", 20, 5);
 	//	Collections.sort(list);	//implements compareTo() method and sorts by increasing order of average ratings
 		System.out.println("RATING"+"\t"+"MOVIE TITLE");
 		for(Rating currRating : list){
@@ -72,11 +72,135 @@ public class MovieRunnerSimilarRatings {
 	//System.out.println("Number of movies that have more than specified ratings : "+list.size());
 	}
 	
+	private void printSimilarRatingsByGenre(){
+		RaterDatabase.initialize("ratings.csv");
+		MovieDatabase.initialize("ratedmoviesfull.csv");
+		int numberOfMovies = MovieDatabase.size();
+		int numberOfRaters = RaterDatabase.size();
+		System.out.println("Total number of movies loaded :"+numberOfMovies);
+		System.out.println("Total number of raters loaded :"+numberOfRaters);
+		System.out.println("------------------------------------------------------------------");
+		
+		
+		GenreFilter genreFilter = new GenreFilter("Mystery");
+		
+		
+		FourthRatings fr = new FourthRatings();
+		ArrayList<Rating> list = fr.getSimilarRatingsByFilter("964", 20, 5, genreFilter);
+		System.out.println("Number of movies of the specified genre : "+list.size());
+		
+	//	Collections.sort(list);	//implements compareTo() method and sorts by increasing order of average ratings
+		System.out.println("RATING"+"\t"+"MOVIE TITLE"+"\t\t"+"GENRE(s)");
+		for(Rating currRating : list){
+			System.out.println(currRating.getValue()+"\t"+MovieDatabase.getTitle(currRating.getItem())+"\t\t"+
+		MovieDatabase.getGenres(currRating.getItem()));
+		}
+		System.out.println("------------------------------------------------------------------");
+		
+	//System.out.println("Number of movies that have more than specified ratings : "+list.size());
+	}
+	
+	private void printSimilarRatingsByDirector(){
+		RaterDatabase.initialize("ratings.csv");
+		MovieDatabase.initialize("ratedmoviesfull.csv");
+		int numberOfMovies = MovieDatabase.size();
+		int numberOfRaters = RaterDatabase.size();
+		System.out.println("Total number of movies loaded :"+numberOfMovies);
+		System.out.println("Total number of raters loaded :"+numberOfRaters);
+		System.out.println("------------------------------------------------------------------");
+		
+		
+		DirectorsFilter DirectorFilter = new DirectorsFilter(
+				"Clint Eastwood,J.J. Abrams,Alfred Hitchcock,Sydney Pollack,David Cronenberg,Oliver Stone,Mike Leigh");
+		
+		FourthRatings fr = new FourthRatings();
+		ArrayList<Rating> list = fr.getSimilarRatingsByFilter("120", 10, 2, DirectorFilter);
+		System.out.println("Number of movies of the specified genre : "+list.size());
+		
+	//	Collections.sort(list);	//implements compareTo() method and sorts by increasing order of average ratings
+		System.out.println("RATING"+"\t"+"MOVIE TITLE"+"\t\t"+"DIRECTOR(s)");
+		for(Rating currRating : list){
+			System.out.println(currRating.getValue()+"\t"+MovieDatabase.getTitle(currRating.getItem())
+					+"\t\t"+MovieDatabase.getDirector(currRating.getItem()));
+		}
+		System.out.println("------------------------------------------------------------------");
+		
+	//System.out.println("Number of movies that have more than specified ratings : "+list.size());
+	}
+	
+	
+	private void printSimilarRatingsByGenreAndMinutes(){
+		RaterDatabase.initialize("ratings.csv");
+		MovieDatabase.initialize("ratedmoviesfull.csv");
+		int numberOfMovies = MovieDatabase.size();
+		int numberOfRaters = RaterDatabase.size();
+		System.out.println("Total number of movies loaded :"+numberOfMovies);
+		System.out.println("Total number of raters loaded :"+numberOfRaters);
+		System.out.println("------------------------------------------------------------------");
+		
+		
+		AllFilters af = new AllFilters();
+		af.addFilter(new MinutesFilter(80, 160));
+		af.addFilter(new GenreFilter("Drama"));
+		
+		FourthRatings fr = new FourthRatings();
+		ArrayList<Rating> list = fr.getSimilarRatingsByFilter("168", 10, 3, af);
+		System.out.println("Number of movies of the specified genre : "+list.size());
+		
+	//	Collections.sort(list);	//implements compareTo() method and sorts by increasing order of average ratings
+		System.out.println("RATING"+"\t"+"MINUTES"+"\t"+"MOVIE TITLE"+"\t\t"+"GENRE(s)");
+		for(Rating currRating : list){
+			System.out.println(currRating.getValue()+"\t"+MovieDatabase.getMinutes(currRating.getItem())
+					+"\t"+MovieDatabase.getTitle(currRating.getItem())
+							+"\t\t"+MovieDatabase.getGenres(currRating.getItem()));
+		}
+		System.out.println("------------------------------------------------------------------");
+		
+	//System.out.println("Number of movies that have more than specified ratings : "+list.size());
+	}
+	
+	
+	private void printSimilarRatingsByYearAfterAndMinutes(){
+		RaterDatabase.initialize("ratings.csv");
+		MovieDatabase.initialize("ratedmoviesfull.csv");
+		int numberOfMovies = MovieDatabase.size();
+		int numberOfRaters = RaterDatabase.size();
+		System.out.println("Total number of movies loaded :"+numberOfMovies);
+		System.out.println("Total number of raters loaded :"+numberOfRaters);
+		System.out.println("------------------------------------------------------------------");
+		
+		
+		AllFilters af = new AllFilters();
+		af.addFilter(new YearAfterFilter(1975));
+		af.addFilter(new MinutesFilter(70, 200));
+		
+		FourthRatings fr = new FourthRatings();
+		ArrayList<Rating> list = fr.getSimilarRatingsByFilter("314", 10, 5, af);
+		System.out.println("Number of movies of the specified genre : "+list.size());
+		
+	//	Collections.sort(list);	//implements compareTo() method and sorts by increasing order of average ratings
+		System.out.println("RATING"+"\t"+"YEAR"+"\t"+"MINUTES"+"\t"+"MOVIE TITLE");
+		for(Rating currRating : list){
+			System.out.println(currRating.getValue()+"\t"+MovieDatabase.getYear(currRating.getItem())+"\t"+
+		MovieDatabase.getMinutes(currRating.getItem())+"\t"+MovieDatabase.getTitle(currRating.getItem()));
+		}
+		System.out.println("------------------------------------------------------------------");
+		
+	//System.out.println("Number of movies that have more than specified ratings : "+list.size());
+	}
+	
+	
+	
 	
 	public static void main(String[] args) {
 		MovieRunnerSimilarRatings mrsr = new MovieRunnerSimilarRatings();
 	//	mrsr.printAverageRatings();
 	//	mrsr.printAverageRatingsByYearAfterAndGenre();
-		mrsr.printSimilarRatings();
+	//	mrsr.printSimilarRatings();
+	//	mrsr.printSimilarRatingsByGenre();
+	//	mrsr.printSimilarRatingsByDirector();
+	//	mrsr.printSimilarRatingsByGenreAndMinutes();
+		mrsr.printSimilarRatingsByYearAfterAndMinutes();
+		
 	}
 }
